2be3281f320bf063ff8b9993dda41d1c1f65a119
Harsh Jivani <133632112+harsh8104@users.noreply.github.com>
2024-08-04 22:27:45 +0530

    Update List.jsx

--- admin/latest/src/pages/List/List.jsx
+++ admin/latest/src/pages/List/List.jsx
@@ -4,10 +4,13 @@ import { toast } from "react-toastify";
 import "./List.css";
 const List = ({ url }) => {
   const [list, setList] = useState([]);
+  const [loading, setLoading] = useState(false);
   const fetchList = async () => {
+    setLoading(true);
     const response = await axios.get(`${url}/api/food/list`);
     if (response.data.success) {
       setList(response.data.data);
+      setLoading(false);
     } else {
       toast.error("Error");
     }
@@ -26,34 +29,40 @@ const List = ({ url }) => {
   useEffect(() => {
     fetchList();
   }, []);
-  console.log(list);
-  return (
-    <div className="list add flex-col">
-      <p>All Foods List</p>
-      <div className="list-table">
-        <div className="list-table-format title">
-          <b>Image</b>
-          <b>Name</b>
-          <b>Category</b>
-          <b>Price</b>
-          <b>Action</b>
+  if (!loading) {
+    return (
+      <div className="list add flex-col">
+        <p>All Foods List</p>
+        <div className="list-table">
+          <div className="list-table-format title">
+            <b>Image</b>
+            <b>Name</b>
+            <b>Category</b>
+            <b>Price</b>
+            <b>Action</b>
+          </div>
+          {list.map((item, index) => {
+            return (
+              <div className="list-table-format" key={index}>
+                <img src={`${url}/images/` + item.img} alt="" />
+                <p>{item.name}</p>
+                <p>{item.category}</p>
+                <p>${item.price}</p>
+                <p onClick={() => removeFood(item._id)} className="cursor">
+                  X
+                </p>
+              </div>
+            );
+          })}
         </div>
-        {list.map((item, index) => {
-          return (
-            <div className="list-table-format" key={index}>
-              <img src={`${url}/images/` + item.img} alt="" />
-              <p>{item.name}</p>
-              <p>{item.category}</p>
-              <p>${item.price}</p>
-              <p onClick={() => removeFood(item._id)} className="cursor">
-                X
-              </p>
-            </div>
-          );
-        })}
       </div>
-    </div>
-  );
+    );
+  } else
+    return (
+      <div className="spinner">
+        <div className="spin"></div>
+      </div>
+    );
 };
 
 export default List;
